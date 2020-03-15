a884c33bf7172d4c7f0fcad1d15efdd44e9df58c
agata-nowicka <agata.tabaszewska@gmail.com>
2020-03-15 19:56:19 +0100

    first ready

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Get the button:
-mybutton = document.getElementById("topBtn");
+//mybutton = document.getElementById("topBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("topBtn").style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    document.getElementById("topBtn").style.display = "none";
   }
 } 
 
